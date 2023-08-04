c0c7adc7ccf50954d7c70d1361c729a4fbf9aae6
Nial Johnson <nialj707@gmail.com>
2023-08-03 20:47:35 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,16 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>I do not know how to code like a pro</p>
+    <img src={image} alt="I made this"></img>
+
+
+
+
+
+  </div>
 }
 
 export default About;
